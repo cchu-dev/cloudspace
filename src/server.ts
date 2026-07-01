dc0b3166a85634e982e49bf252f7f68a22228812
cchu-dev <284191038+cchu-dev@users.noreply.github.com>
2026-07-01 04:32:12 +0000

    fix trust proxy issue

--- src/server.ts
+++ src/server.ts
@@ -1571,7 +1571,7 @@ export function createServer(config = loadConfig()): RunningServer {
   const processSessions = new ProcessSessionManager();
 
   if (config.logging.trustProxy) {
-    app.set("trust proxy", true);
+    app.set("trust proxy", 1);
   }
 
   app.use((req, res, next) => {
